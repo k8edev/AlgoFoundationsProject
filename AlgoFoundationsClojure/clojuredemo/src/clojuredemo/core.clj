e570dba1d16577238cefa470f07e1463751b4c09
ccowden <catie.cowden@gmail.com>
2021-08-07 16:07:35 -0700

    More Clojure code and tests

--- AlgoFoundationsClojure/clojuredemo/src/clojuredemo/core.clj
+++ AlgoFoundationsClojure/clojuredemo/src/clojuredemo/core.clj
@@ -1,7 +1,7 @@
 (ns src.clojuredemo.core)
 
 (defn tail-recursive-fibonacci
-  "Tail-recursive version of the fibonacci sequence."
+  "Tail-recursive version of the Fibonacci sequence."
   [n]
   (if (> n 1)
     (loop [x 1
@@ -15,9 +15,17 @@
         fib1))
     n))
 
+(defn recursive-fibonacci
+  "Recursive implementation of the Fibonacci sequence."
+  [n]
+  (cond
+    (= n 0) 0
+    (= n 1) 1
+    :else (+ (recursive-fibonacci (- n 1))
+             (recursive-fibonacci (- n 2)))))
 
-(defn factorial-using-recur
-  "Recursive version of the factorial algorithm"
+(defn tail-recursive-factorial
+  "Tail-recursive implementation of the factorial algorithm"
   [n]
   (loop [current n
          next (dec current)
@@ -25,3 +33,11 @@
     (if (> current 1)
       (recur next (dec next) (* total current))
       total)))
+
+(defn recursive-factorial
+  "Recursive implementation of the factorial algorithm"
+  [n]
+  (if (= n 1)
+    1
+    (* n (recursive-factorial (- n 1)))))
+
